7452b4e51e345d4f819637a9d96d22008918d9bd
fkubota <rkkubotay@gmail.com>
2020-02-23 16:23:58 +0900

    update fkubota01(fourier) edit key semicolon

--- keyboards/keebio/fourier/keymaps/fkubota01/keymap.c
+++ keyboards/keebio/fourier/keymaps/fkubota01/keymap.c
@@ -28,7 +28,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  KC_BSPC,
-    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN,          KC_ENT,
+    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_ENT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, KC_RGUI,
     KC_LCTL,  KC_LGUI, KC_LALT, _______, SPFN1,            SPFN2,                     KC_RALT, KC_0,    KC_RGUI, KC_RCTL
   ),
@@ -80,19 +80,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	  tap_code(KC_LEFT);
 	  tap_code(KC_LEFT);
 	  tap_code(KC_LEFT);
-	  tap_code(KC_LEFT);
-	  tap_code(KC_LEFT);
-	  tap_code(KC_LEFT);
-	  tap_code(KC_LEFT);
-	  tap_code(KC_LEFT);
       break;
 
     case M_RMASH:
-	  tap_code(KC_RGHT);
-	  tap_code(KC_RGHT);
-	  tap_code(KC_RGHT);
-	  tap_code(KC_RGHT);
-	  tap_code(KC_RGHT);
 	  tap_code(KC_RGHT);
 	  tap_code(KC_RGHT);
 	  tap_code(KC_RGHT);
